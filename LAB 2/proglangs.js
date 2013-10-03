efe9d2b5e391fcf8ea4106fd9c07c95e773ba556
erikm13 <erikmikalsen94@gmail.com>
2013-10-03 20:20:30 +0200

    LAB 2 Innlevering
    La til javascript løsning for å fortelle brukeren at sidene er under
    konstruksjon. Rettet også litt på CSS på navigeringsmenyen.

--- LAB 2/proglangs.js
+++ LAB 2/proglangs.js
@@ -3,3 +3,7 @@ function printToOutputBox(selectobj){
  var e_outputbox = document.getElementById("outputbox");
  e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
  }
+  function konstruksjon ()
+{
+alert("Denne siden er under konstruksjon. Vi beklager dette.");
+}
